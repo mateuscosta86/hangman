8c69fd405b8506c4c89b66dc983f8fb63d83ab0a
Mateus Cavalcanti <mateus.cavalcanti@socialminer.com>
2022-01-12 22:07:57 -0300

    supervised dictionary agent

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,14 +1,19 @@
 defmodule Dictionary.Runtime.Server do
+  use Agent
   alias Dictionary.Impl.WordList
 
   @type t :: pid
 
-  @spec start_link :: {:error, any} | {:ok, pid}
-  def start_link() do
+  @spec start_link(any) :: {:error, any} | {:ok, pid}
+  def start_link(_init) do
     Agent.start_link(&WordList.word_list/0, name: __MODULE__)
   end
 
   def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(__MODULE__, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(__MODULE__, &WordList.random_word/1)
   end
 end
