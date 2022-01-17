c8e39b71f9f3f716100cd1a8bd06bb5141343bbe
Mateus Cavalcanti <mateus.cavalcanti@socialminer.com>
2022-01-16 22:30:19 -0300

    using genserver

--- core/lib/core.ex
+++ core/lib/core.ex
@@ -1,12 +1,19 @@
 defmodule Core do
-  alias Core.Impl.Game
+  alias Core.Runtime.Server
 
-  @spec new_game :: Game.t()
-  defdelegate new_game, to: Game
+  @spec new_game :: Server.t()
+  def new_game do
+    {:ok, pid} = Server.start_link()
+    pid
+  end
 
-  @spec make_move(Game.t(), String.t()) :: {Game.t(), Type.tally()}
-  defdelegate make_move(game, guess), to: Game
+  @spec make_move(Server.t(), String.t()) :: {Server.t(), Type.tally()}
+  def make_move(game, guess) do
+    GenServer.call(game, {:make_move, guess})
+  end
 
-  @spec tally(Game.t()) :: Type.tally()
-  defdelegate tally(game), to: Game
+  @spec tally(Server.t()) :: Type.tally()
+  def tally(game) do
+    GenServer.call(game, {:tally})
+  end
 end
---
+++ core/lib/runtime/server.ex
@@ -0,0 +1,24 @@
+defmodule Core.Runtime.Server do
+  use GenServer
+  alias Core.Impl.Game
+
+  @type t :: pid()
+
+  @spec start_link :: :ignore | {:error, any} | {:ok, pid}
+  def start_link() do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    {:ok, Game.new_game()}
+  end
+
+  def handle_call({:make_move, guess}, _from, game) do
+    {updated_game, tally} = Game.make_move(game, guess)
+    {:reply, tally, updated_game}
+  end
+
+  def handle_call({:tally}, _from, game) do
+    {:reply, Game.tally(game), game}
+  end
+end
